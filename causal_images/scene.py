b4074c0d5b17c6ea9f12d090f2b66dad53a62f34
Jonas Bernhard <jonasbernhard96@gmail.com>
2022-07-27 18:25:09 +0200

    Add simple relative positioning

--- causal_images/scene.py
+++ causal_images/scene.py
@@ -16,10 +16,12 @@ class PrimitiveShape(Enum):
 
 
 class RelativePosition(Enum):
-    TOP = "TOP"
-    BOTTOM = "BOTTOM"
+    FRONT = "FRONT"
+    BEHIND = "BEHIND"
     LEFT = "LEFT"
     RIGHT = "RIGHT"
+    BOTTOM = "BOTTOM"
+    TOP = "TOP"
 
 
 @dataclass
@@ -43,10 +45,60 @@ class Scene:
         return position
 
     def set_object_relative_position(
-        self, target_obj_id, reference_obj_id, reference_position, relative_position
+        self, target_obj_id, reference_obj_id, reference_pos, relative_pos
     ):
         reference_obj = self.objects[reference_obj_id].mesh
         target_obj = self.objects[target_obj_id].mesh
+
+        if relative_pos == RelativePosition.FRONT:
+            critical_dim = 0
+            reference_loc_smaller = True
+
+        if relative_pos == RelativePosition.BEHIND:
+            critical_dim = 0
+            reference_loc_smaller = False
+
+        if relative_pos == RelativePosition.RIGHT:
+            critical_dim = 1
+            reference_loc_smaller = True
+
+        if relative_pos == RelativePosition.LEFT:
+            critical_dim = 1
+            reference_loc_smaller = False
+
+        if relative_pos == RelativePosition.TOP:
+            critical_dim = 2
+            reference_loc_smaller = True
+
+        if relative_pos == RelativePosition.BOTTOM:
+            critical_dim = 2
+            reference_loc_smaller = False
+
+        reference_critical_pos = (max if reference_loc_smaller else min)(
+            [loc[critical_dim] for loc in reference_obj.get_bound_box()]
+        )
+        target_critical_pos = (min if reference_loc_smaller else max)(
+            [loc[critical_dim] for loc in target_obj.get_bound_box()]
+        )
+        target_obj.set_location(
+            [
+                reference_critical_pos
+                + target_obj.get_location()[0]
+                - target_critical_pos
+                if critical_dim == 0
+                else reference_pos[0],
+                reference_critical_pos
+                + target_obj.get_location()[1]
+                - target_critical_pos
+                if critical_dim == 1
+                else reference_pos[1],
+                reference_critical_pos
+                + target_obj.get_location()[2]
+                - target_critical_pos
+                if critical_dim == 2
+                else reference_pos[2],
+            ]
+        )
 
     def cleanup(self):
         bproc.object.delete_multiple([obj.mesh for obj in self.objects.values()])
