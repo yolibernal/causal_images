b3609690ed6a0f92ff6e07074fc04fcc00171e8b
Jonas Bernhard <jonasbernhard96@gmail.com>
2022-07-28 12:44:55 +0200

    Add example SCM file

---
+++ examples/scm.py
@@ -0,0 +1,32 @@
+import numpy as np
+from causal_images.scene import PrimitiveShape, RelativePosition
+from causal_images.scm import SceneSCM
+
+scm = SceneSCM(
+    lambda scene: {
+        "obj_x": ([], lambda: scene.create_primitive(PrimitiveShape.CUBE), None),
+        "pos_x": (
+            ["obj_x"],
+            lambda obj_x: scene.set_object_position(
+                obj_x, np.random.normal(loc=0, scale=1, size=(3,))
+            ),
+            None,
+        ),
+        "obj_y": ([], lambda: scene.create_primitive(PrimitiveShape.SPHERE), None),
+        "pos_y": (
+            ["obj_y"],
+            lambda obj_x: scene.set_object_position(
+                obj_x, np.random.normal(loc=5, scale=1, size=(3,))
+            ),
+            None,
+        ),
+        "obj_z": ([], lambda: scene.create_primitive(PrimitiveShape.CONE), None),
+        "pos_z": (
+            ["obj_z", "obj_y", "pos_y"],
+            lambda obj_z, obj_y, pos_y: scene.set_object_relative_position(
+                obj_z, obj_y, pos_y, RelativePosition.TOP
+            ),
+            None,
+        ),
+    }
+)
